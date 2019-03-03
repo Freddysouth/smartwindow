df91ec7c132d9d7a2b0a98db2d38f7f5f171dce5
freddysouth <fredd-sormo-1995@hotmail.com>
2019-03-02 16:55:00 -0800

    Add print statements for debugging purposes, revert to python2 to see if that works better

--- raspberrypi/main.py
+++ raspberrypi/main.py
@@ -52,39 +52,50 @@ def main():
 			time.sleep(1)
 		else:
 
+			print("####Starting####")
 			aqi_client.cmd_set_sleep(0)
 			aqi_client.cmd_set_mode(1)
 
 			for t in range(15):
+				print("####for t in range(15)####")
 				values = aqi_client.cmd_query_data()
 				if values is not None:
 					print("PM2.5: ", values[0], ", PM10: ", values[1])
 					time.sleep(2)
-			
+			print("####Done with for t in range(15)####")
 			# open stored data
 			with open('/var/www/html/aqi.json') as json_data:
 				data = json.load(json_data)
-			
+			print("####open data####")
+			print(data)
+
 			# check if length is more than 100 and delete first element
 			if len(data) > 2:
+				print("####Starting publishing process####")
 				aggregate = aggregator.get_mean(data)
 				prepend = {
 					"device": "window_lars",
 					"device_ip": get_ip_address("wlan0")
+					"pm25": aggregate["pm25"],
+					"pm10": aggregate["pm10"],
+					"timestamp": aggregate["timestamp"]
 				}
-				payload = json.dumps({**prepend, **aggregate})
+				payload = json.dumps(prepend)
 
 				mqtt_client.PublishJsonPayload(payload)
 				
 				data.clear()
 
 
+			print("####appending values####")
 			# append new values
 			data.append({'pm25': values[0], 'pm10': values[1], 'time': time.strftime("%d.%m.%Y %H:%M:%S")})
 
 			# save it
 			with open('/var/www/html/aqi.json', 'w') as outfile:
 				json.dump(data, outfile)
+
+			print("####just wrote file####")
 
 			print("Going to sleep for 5 sec...")
 			aqi_client.cmd_set_mode(0)
