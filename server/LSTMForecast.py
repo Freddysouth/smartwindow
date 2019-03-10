80b2cbae2f930bc8e1399ffb2bed3b501d0a87da
freddysouth <fredd-sormo-1995@hotmail.com>
2019-03-10 15:50:04 -0700

    Fix minor syntax issues

--- server/LSTMForecast.py
+++ server/LSTMForecast.py
@@ -14,178 +14,178 @@ import os
 
 class LSTMForecast:
 
-def __init__(self, values):
-    self.values = values
-    self.train = []
-    self.test = []
-    self.trainInput = []
-    self.trainOutput = []
-    self.testInput = []
-    self.testOutput = []
-    self.model = None
-    self.history = None
-    self.scaler = MinMaxScaler(feature_range=(0, 1))
+    def __init__(self, values):
+        self.values = values
+        self.train = []
+        self.test = []
+        self.trainInput = []
+        self.trainOutput = []
+        self.testInput = []
+        self.testOutput = []
+        self.model = None
+        self.history = None
+        self.scaler = MinMaxScaler(feature_range=(0, 1))
 
-# convert series to supervised learning
-def series_to_supervised(self, n_in=1, n_out=1, dropnan=True):
-    n_vars = 1 if type(self.values) is list else self.values.shape[1]
-    df = DataFrame(self.values)
-    cols, names = list(), list()
-    # input sequence (t-n, ... t-1)
-    for i in range(n_in, 0, -1):
-        cols.append(df.shift(i))
-        names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
-    # forecast sequence (t, t+1, ... t+n)
-    for i in range(0, n_out):
-        cols.append(df.shift(-i))
-        if i == 0:
-            names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
-        else:
-            names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
-    # put it all together
-    agg = concat(cols, axis=1)
-    agg.columns = names
-    # drop rows with NaN values
-    if dropnan:
-        agg.dropna(inplace=True)
-    return agg
- 
-
-def loadDataSet(self, filePath):
-    # load dataset
-    dataset = read_csv('pollution.csv', header=0, index_col=0)
-    return dataset.values
-
-def encodeLabel(self, index)
-    # integer encode direction
-    encoder = LabelEncoder()
-    self.values[:, index]encoder.fit_transform(self.values[:, index])
-
-#values[:, 4] = encodeLabel(values[:, 4])
-
-#values = normalize(values)
-def normalize(self):
-    # ensure all data is float
-    self.values = self.values.astype('float32')
-    # normalize features
-    self.values = self.scaler.fit_transform(self.values)
+    # convert series to supervised learning
+    def series_to_supervised(self, n_in=1, n_out=1, dropnan=True):
+        n_vars = 1 if type(self.values) is list else self.values.shape[1]
+        df = DataFrame(self.values)
+        cols, names = list(), list()
+        # input sequence (t-n, ... t-1)
+        for i in range(n_in, 0, -1):
+            cols.append(df.shift(i))
+            names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+        # forecast sequence (t, t+1, ... t+n)
+        for i in range(0, n_out):
+            cols.append(df.shift(-i))
+            if i == 0:
+                names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+            else:
+                names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+        # put it all together
+        agg = concat(cols, axis=1)
+        agg.columns = names
+        # drop rows with NaN values
+        if dropnan:
+            agg.dropna(inplace=True)
+        return agg
 
 
-# frame as supervised learning
-#reframed = series_to_supervised(scaled, 1, 1)
+    def loadDataSet(self, filePath):
+        # load dataset
+        dataset = read_csv('pollution.csv', header=0, index_col=0)
+        return dataset.values
 
-def dropColumns(self, columnIndexes):
-    self.values.drop(self.values.columns[columnIndexes], axis=1, inplace=True)  
+    def encodeLabel(self, index):
+        # integer encode direction
+        encoder = LabelEncoder()
+        self.values[:, index] = encoder.fit_transform(self.values[:, index])
 
-# drop columns we don't want to predict
-#reframed = dropColumns([9,10,11,12,13,14,15], 1, True)
+    #values[:, 4] = encodeLabel(values[:, 4])
 
-#print(reframed.head())
-def splitTrainTestSets(self, trainRatio):
-    self.train = self.values[:int(len(self.values) * trainRatio), :]
-    self.test = self.values[int(len(self.values)* trainRatio):, :]
+    #values = normalize(values)
+    def normalize(self):
+        # ensure all data is float
+        self.values = self.values.astype('float32')
+        # normalize features
+        self.values = self.scaler.fit_transform(self.values)
 
-# split into train and test sets
-#values = reframed.values
-#n_train_hours = 365 * 24
-#train = values[:n_train_hours, :]
-#test = values[n_train_hours:, :]
-#train, test = split(0.8, values)
-def splitInputOutput(self):
-    self.trainInput, self.trainOutput = self.train[:, :-1], self.train[:, -1]
-    self.testInput, self.testOutput = self.test[:, :-1], self.test[:, -1]
-# split into input and outputs
-#train_X, train_y = train[:, :-1], train[:, -1]
-#test_X, test_y = test[:, :-1], test[:, -1]
 
-def reshapeTrainTestSetsTo3d(self):
-    self.trainInput = self.trainInput.reshape((trainInput.shape[0], 1, trainInput.shape[1]))
-    self.testInput = self.testInput.reshape((testInput.shape[0], 1, testInput.shape[1]))
+    # frame as supervised learning
+    #reframed = series_to_supervised(scaled, 1, 1)
 
-# reshape input to be 3D [samples, timesteps, features]
-#train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
-#test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
-#print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
+    def dropColumns(self, columnIndexes):
+        self.values.drop(self.values.columns[columnIndexes], axis=1, inplace=True)
 
-def saveModel(self, filePath):
-    self.model.save(filePath)
-
-def initModel(self):
-    self.model = Sequential()
-    self.model.add(LSTM(50, input_shape=(self.trainInput.shape[1], self.trainInput.shape[2])))
-    self.model.add(Dense(1))
-    self.model.compile(loss='mae', optimizer='adam')
-
-def fitModel(self, filePath):
-    self.history = self.model.fit(trainInput, trainOutput, epochs=50, batch_size=72, validation_data=(testInput, testOutput), verbose=2, shuffle=False) 
-    self.saveModel(filePath)
-#history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
-
-def reshapePredictionInputTo3d(self, predictionInput):
-    return predictionInput.reshape((predictionInput.shape[0], 1, predictionInput.shape[1])) 
-
-def getNormalizedPrediction(self, predictionInput):
-    predictionInput = self.reshapePredictionInputTo3d(predictionInput)
-    return self.model.predict(predictionInput)
-
-def getActualPrediction(self, predictionInput, prediction):
-    predictionInput = predictionInput.reshape((predictionInput.shape[0], predictionInput.shape[2]))
-    
-    actualizedPrediction = concatenate((prediction, predictionInput[:, 1:]), axis=1)
-    actualizedPrediction = self.scaler.inverse_transform(actualizedPrediction)
-    return actualizedPrediction[:,0]
-
-def train(self, filePath, encodeIndexes, columnsToDrop)
-    # encode potential categorical columns
-    if encodeIndexes:
-        for i in encodeIndexes:
-            self.encodeLabel(i)
-    # normalize all values in dataset
-    self.normalize()
     # drop columns we don't want to predict
-    self.dropColumns(columnsToDrop)
-    # split dataset into train and test sets
-    self.splitTrainTestSets(0.8)
-    # split train and test sets to representative inputs and outputs
-    self.splitInputOutput()
-    self.reshapeTrainTestSetsTo3d()
-    # prepare, fit, and save model
-    self.initModel()
-    self.fitModel(filePath)
+    #reframed = dropColumns([9,10,11,12,13,14,15], 1, True)
 
-def init(train=False, filePath, encodeIndexes, columnsToDrop):
-    if train:
-        self.train(filePath, encodeIndexes, columnsToDrop)
-    else:
-        if os.path.exists(filePath):
-            self.model = keras.models.load_model(filePath)
-        else:
+    #print(reframed.head())
+    def splitTrainTestSets(self, trainRatio):
+        self.train = self.values[:int(len(self.values) * trainRatio), :]
+        self.test = self.values[int(len(self.values)* trainRatio):, :]
+
+    # split into train and test sets
+    #values = reframed.values
+    #n_train_hours = 365 * 24
+    #train = values[:n_train_hours, :]
+    #test = values[n_train_hours:, :]
+    #train, test = split(0.8, values)
+    def splitInputOutput(self):
+        self.trainInput, self.trainOutput = self.train[:, :-1], self.train[:, -1]
+        self.testInput, self.testOutput = self.test[:, :-1], self.test[:, -1]
+    # split into input and outputs
+    #train_X, train_y = train[:, :-1], train[:, -1]
+    #test_X, test_y = test[:, :-1], test[:, -1]
+
+    def reshapeTrainTestSetsTo3d(self):
+        self.trainInput = self.trainInput.reshape((trainInput.shape[0], 1, trainInput.shape[1]))
+        self.testInput = self.testInput.reshape((testInput.shape[0], 1, testInput.shape[1]))
+
+    # reshape input to be 3D [samples, timesteps, features]
+    #train_X = train_X.reshape((train_X.shape[0], 1, train_X.shape[1]))
+    #test_X = test_X.reshape((test_X.shape[0], 1, test_X.shape[1]))
+    #print(train_X.shape, train_y.shape, test_X.shape, test_y.shape)
+
+    def saveModel(self, filePath):
+        self.model.save(filePath)
+
+    def initModel(self):
+        self.model = Sequential()
+        self.model.add(LSTM(50, input_shape=(self.trainInput.shape[1], self.trainInput.shape[2])))
+        self.model.add(Dense(1))
+        self.model.compile(loss='mae', optimizer='adam')
+
+    def fitModel(self, filePath):
+        self.history = self.model.fit(trainInput, trainOutput, epochs=50, batch_size=72, validation_data=(testInput, testOutput), verbose=2, shuffle=False)
+        self.saveModel(filePath)
+    #history = model.fit(train_X, train_y, epochs=50, batch_size=72, validation_data=(test_X, test_y), verbose=2, shuffle=False)
+
+    def reshapePredictionInputTo3d(self, predictionInput):
+        return predictionInput.reshape((predictionInput.shape[0], 1, predictionInput.shape[1]))
+
+    def getNormalizedPrediction(self, predictionInput):
+        predictionInput = self.reshapePredictionInputTo3d(predictionInput)
+        return self.model.predict(predictionInput)
+
+    def getActualPrediction(self, predictionInput, prediction):
+        predictionInput = predictionInput.reshape((predictionInput.shape[0], predictionInput.shape[2]))
+
+        actualizedPrediction = concatenate((prediction, predictionInput[:, 1:]), axis=1)
+        actualizedPrediction = self.scaler.inverse_transform(actualizedPrediction)
+        return actualizedPrediction[:,0]
+
+    def train(self, filePath, encodeIndexes, columnsToDrop):
+        # encode potential categorical columns
+        if encodeIndexes:
+            for i in encodeIndexes:
+                self.encodeLabel(i)
+        # normalize all values in dataset
+        self.normalize()
+        # drop columns we don't want to predict
+        self.dropColumns(columnsToDrop)
+        # split dataset into train and test sets
+        self.splitTrainTestSets(0.8)
+        # split train and test sets to representative inputs and outputs
+        self.splitInputOutput()
+        self.reshapeTrainTestSetsTo3d()
+        # prepare, fit, and save model
+        self.initModel()
+        self.fitModel(filePath)
+
+    def init(self, filePath, encodeIndexes, columnsToDrop, train):
+        if train:
             self.train(filePath, encodeIndexes, columnsToDrop)
+        else:
+            if os.path.exists(filePath):
+                self.model = keras.models.load_model(filePath)
+            else:
+                self.train(filePath, encodeIndexes, columnsToDrop)
 
-#input format: [[weatherparameters...., previosPollution]]
-def predict(self, inputData):
-    normalizedPrediction = self.getNormalizedPrediction(inputData)
-    actualPrediction = self.getActualPrediction(inputData, normalizedPrediction)
+    #input format: [[weatherparameters...., previosPollution]]
+    def predict(self, inputData):
+        normalizedPrediction = self.getNormalizedPrediction(inputData)
+        actualPrediction = self.getActualPrediction(inputData, normalizedPrediction)
 
-    return actualPrediction
+        return actualPrediction
 
 
-#test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
+    #test_X = test_X.reshape((test_X.shape[0], test_X.shape[2]))
 
-#inv_yhat = concatenate((yhat, test_X[:, 1:]), axis=1)
-#inv_yhat = scaler.inverse_transform(inv_yhat)
-#inv_yhat = inv_yhat[:,0]
+    #inv_yhat = concatenate((yhat, test_X[:, 1:]), axis=1)
+    #inv_yhat = scaler.inverse_transform(inv_yhat)
+    #inv_yhat = inv_yhat[:,0]
 
-#print(inv_yhat)
+    #print(inv_yhat)
 
-#test_y = test_y.reshape((len(test_y), 1))
-#inv_y = concatenate((test_y, test_X[:, 1:]), axis=1)
-#inv_y = scaler.inverse_transform(inv_y)
-#inv_y = inv_y[:,0]
+    #test_y = test_y.reshape((len(test_y), 1))
+    #inv_y = concatenate((test_y, test_X[:, 1:]), axis=1)
+    #inv_y = scaler.inverse_transform(inv_y)
+    #inv_y = inv_y[:,0]
 
-#rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
-#print('Test RMSE: %.3f' % rmse)
+    #rmse = sqrt(mean_squared_error(inv_y, inv_yhat))
+    #print('Test RMSE: %.3f' % rmse)
 
-#model.save('model.h5')
+    #model.save('model.h5')
 
 
--- server/server.py
+++ server/server.py
@@ -20,52 +20,52 @@ model = None
 
 
 def load_model():
-		# load the pre-trained Keras model (here we are using a model
-		# pre-trained on ImageNet and provided by Keras, but you can
-		# substitute in your own networks just as easily)
-		global model
-		model = keras.models.load_model("model.h5")
+    # load the pre-trained Keras model (here we are using a model
+    # pre-trained on ImageNet and provided by Keras, but you can
+    # substitute in your own networks just as easily)
+    global model
+    model = keras.models.load_model("model.h5")
 
 
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
-	n_vars = 1 if type(data) is list else data.shape[1]
-	df = DataFrame(data)
-	cols, names = list(), list()
-	# input sequence (t-n, ... t-1)
-	for i in range(n_in, 0, -1):
-		cols.append(df.shift(i))
-		names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
-	# forecast sequence (t, t+1, ... t+n)
-	for i in range(0, n_out):
-		cols.append(df.shift(-i))
-		if i == 0:
-			names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
-		else:
-			names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
-	# put it all together
-	agg = concat(cols, axis=1)
-	agg.columns = names
-	# drop rows with NaN values
-	if dropnan:
-		agg.dropna(inplace=True)
-	return agg
- 
+  n_vars = 1 if type(data) is list else data.shape[1]
+  df = DataFrame(data)
+  cols, names = list(), list()
+  # input sequence (t-n, ... t-1)
+  for i in range(n_in, 0, -1):
+    cols.append(df.shift(i))
+    names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
+  # forecast sequence (t, t+1, ... t+n)
+  for i in range(0, n_out):
+    cols.append(df.shift(-i))
+    if i == 0:
+      names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
+    else:
+      names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
+  # put it all together
+  agg = concat(cols, axis=1)
+  agg.columns = names
+  # drop rows with NaN values
+  if dropnan:
+    agg.dropna(inplace=True)
+  return agg
+
 
 
 def getWeather():
-	response = requests.get("https://weather.cit.api.here.com/weather/1.0/report.json?product=forecast_7days&zipcode=10025&oneobservation=true&app_id=Is7EbpLYqp7H4EUwNyMz&app_code=sdwekigD9nSSWlQBa0pZ3g")
-	data = response.json()
-	return json.dumps(data)
+  response = requests.get("https://weather.cit.api.here.com/weather/1.0/report.json?product=forecast_7days&zipcode=10025&oneobservation=true&app_id=Is7EbpLYqp7H4EUwNyMz&app_code=sdwekigD9nSSWlQBa0pZ3g")
+  data = response.json()
+  return json.dumps(data)
 
 @app.route("/predict", methods=["POST"])
 def predict():
-	pass
+  pass
 
 if __name__ == "__main__":
-		print(("* Loading Keras model and Flask starting server..."
-				"please wait until server has fully started"))
-		dataset = read_csv('pollution.csv', header=0, index_col=0)
-	predictor = LSTMForecast(dataset.values)
-	predictor.init(True, 'models/model.h5', [4], [9,10,11,12,13,14,15])
+  print(("* Loading Keras model and Flask starting server..."
+      "please wait until server has fully started"))
+  dataset = read_csv('pollution.csv', header=0, index_col=0)
+  predictor = LSTMForecast(dataset.values)
+  predictor.init('models/model.h5', [4], [9,10,11,12,13,14,15], True)
 
-		app.run()
+  app.run()
