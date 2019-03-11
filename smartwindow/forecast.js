60a5a4f0428e4a54fc84e805dfea82411e55e3bd
Silje <silje@169-231-148-249.wireless.ucsb.edu>
2019-03-10 17:38:32 -0700

    all elements (text and picture) corresponds to the pollution input

---
+++ smartwindow/forecast.js
@@ -0,0 +1,31 @@
+var values = [0.2,1,3,2,4,5,6];
+var lowmedhigh = ["low", "high", "low", "medium", "low", "medium", "low"];
+
+function pollution(listOfValues, listOfLevel){
+    console.log("Kjører")
+	var i;
+	for (i = 0; i < 7; i++){
+		var n = i.toString();
+        document.getElementsByClassName("card-text")[i].innerHTML = listOfValues[i];
+        document.getElementsByClassName("text-muted")[i].innerHTML = lowmedhigh[i];
+        
+        if (listOfLevel[i] == "low"){
+            document.getElementsByClassName("card-footer")[i].id="green";
+            var image = document.getElementsByClassName("card-img-top")[i];
+            image.src = "images/wideopen_brown.png"
+        }
+        else if (listOfLevel[i] == "medium"){
+             document.getElementsByClassName("card-footer")[i].id="yellow";
+            var image = document.getElementsByClassName("card-img-top")[i];
+            image.src = "images/open_brown.png"
+        }
+        else if (listOfLevel[i] == "high"){
+            document.getElementsByClassName("card-footer")[i].id="red";
+            var image = document.getElementsByClassName("card-img-top")[i];
+            image.src = "images/close_brown.png"
+        }
+         
+	}
+}
+
+pollution(values, lowmedhigh);
